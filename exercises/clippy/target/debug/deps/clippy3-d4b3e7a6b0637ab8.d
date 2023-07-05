05e8a9ffd36b69d4d66a4365186f77b9e7a2ae6d
TendouArisu <129414927+TendouArisu@users.noreply.github.com>
2023-07-05 16:56:09 +0800

    Add files via upload

---
+++ exercises/clippy/target/debug/deps/clippy3-d4b3e7a6b0637ab8.d
@@ -0,0 +1,8 @@
+C:\Windows\System32\rustlings\exercises\clippy\target\debug\deps\clippy3-d4b3e7a6b0637ab8.rmeta: clippy3.rs Cargo.toml
+
+C:\Windows\System32\rustlings\exercises\clippy\target\debug\deps\clippy3-d4b3e7a6b0637ab8.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
